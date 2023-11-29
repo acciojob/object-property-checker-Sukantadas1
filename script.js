00cf6c114cccd507283edaf52ae89ad6ef3df325
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-29 10:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+   // Check if the key is a string and not empty
+  if (typeof key !== 'string' || key.length === 0) {
+    // Return false for invalid input
+    return false;
+  }
+
+  // Check if the global object sampleObject contains the given key
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
 // Do not change the code below
